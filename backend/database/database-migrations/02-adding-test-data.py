2dc064d47e6bb3625c19a8655f4940f764c026c2
J-Mill3 <j.miller00579@gmail.com>
2025-04-07 00:01:29 -0400

    Initial commit after getting everything working with the yoyo migrations and such. Updated requirements and the csv files. Added the python file for adding test data to the database

---
+++ backend/database/database-migrations/02-adding-test-data.py
@@ -0,0 +1,19 @@
+# 02-adding-test-data.py
+
+"""
+Create initial schema for the bulletin board
+"""
+
+from yoyo import step
+
+steps = [
+    step("""
+
+    """),
+    step("""
+
+    """),
+    step("""
+
+    """)
+]
